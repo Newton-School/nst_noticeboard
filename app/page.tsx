26e7a3f0c398d60a1cb70f4dd89ed8ba712718ef
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-08-06 10:07:18 +0530

    feat: update authorization handling to redirect unauthorized users to signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { auth } from "@/auth"
 import { getDb } from "@/lib/db";
 import HomePage from "@/components/HomePage";
+import { redirect } from 'next/navigation';
 
 async function HomeSever() {
 
   const session = await auth();
-  if(!session || session.user.role !== "admin")
-    throw new Error("Unauthorized access")
+  if(!session || session.user.role !== "admin"){
+    redirect('/signin')
+  }
 
   const db = await getDb();
 
